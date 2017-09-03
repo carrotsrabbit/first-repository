dfb706f6798c1bfd0b6e5485325f6146a5536be6
carrotsrabbit <Carri_wei@163.com>
2017-09-03 22:59:27 +0800

    review

---
+++ hello.py
@@ -0,0 +1,37 @@
+print('hello')
+
+
+print(1, 2, 3)
+
+a = input('write something')
+
+print(a)
+
+b = 1.2345
+
+print(b)
+
+print(True and False)
+
+print('i\'m choushazi')
+
+print(r'i/m LLL')
+
+print('''a
+b
+c''')
+
+
+print(5/2)
+
+print(5//2)
+
+print(5 % 2)
+
+print(None)
+
+
+
+
+
+
